9debdca60aec09b442957b31e3976fbfb9dcd57a
SamuelitoHerrera <32346344+SamuelitoHerrera@users.noreply.github.com>
2017-12-03 00:38:39 -0400

    GreatArt
    Samuel Herrera 11-SIIN-1-013
    Programacion estructurada C#
    0463
    Programa que calcula si el estudiante aprueba o no una materia basado en su promedio del 1 al 10

---
+++ GreatArt/GreatArt/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Programa para calcular el promedio de un alumno\n");
+            Console.Write("11-SIIN-1-013\n");
+            Console.Write("Samuel Herrera\n");
+
+            int nota1, nota2, nota3;
+
+            string linea;
+
+            Console.Write("Ingrese la primer nota");
+            linea = Console.ReadLine();
+            nota1 = int.Parse(linea);
+
+            Console.Write("Ingrese la segunda nota");
+            linea = Console.ReadLine();
+            nota2 = int.Parse(linea);
+
+            Console.Write("Ingrese la tercera nota");
+            linea = Console.ReadLine();
+            nota3 = int.Parse(linea);
+
+            int promedio;
+
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            if (promedio >= 7)
+            {
+                Console.Write("promocionado");
+            }
+            else
+            {
+                Console.Write("reprobado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
